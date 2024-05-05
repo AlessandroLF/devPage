054a4a35eca1687c93297a97a004e6389ece08c7
AlessandroLF <lombardoalessandro25@hotmail.com>
2024-05-05 14:51:55 -0600

    Server structure

--- app.js
+++ app.js
@@ -1,1 +1,71 @@
-console.log("Hello");
+const http = require("http");
+const path = require("path");
+const fs = require("fs");
+
+const server = http.createServer((req, res) => {
+  console.log("Request: " + req.url);
+  let contentType = "text/html";
+//Router, decide wich code to run for the requested url
+  switch(req.url){
+    case "/": //Home page
+      res.writeHead(200, { "Content-Type": contentType });
+      fs.readFile(path.join("public", "build", "index.html"), (err, content) => {
+        if(err){
+            console.log(err);
+            res.writeHead(500);
+            res.end(`Server Error: ${err.code}`);
+        }else
+            res.end(content);
+      });
+      break;
+
+      //REST API here
+
+    default:{ //If the request is not a reserved word return the requested asset if it's public
+
+      //Correctly set the response's content type
+      let extname = path.extname(req.url);
+      switch (extname) {
+        case ".js":
+          contentType = "text/javascript";
+          break;
+        case ".css":
+          contentType = "text/css";
+          break;
+        case ".json":
+          contentType = "application/json";
+          break;
+        case ".png":
+          contentType = "image/png";
+          break;
+        case ".jpg":
+          contentType = "image/jpg";
+          break;
+      }
+      if (contentType == "text/html" && extname == "") req.url += ".html";
+
+      fs.readFile(path.join(__dirname, "public", req.url), (err, content) => {
+        if (err) {
+          if (err.code == "ENOENT") {
+            fs.readFile(path.join("public", "404.html"), (err, content) => {
+                res.writeHead(404, { "Content-Type": "text/html" });
+                res.end(content, "utf8");
+              }
+            );
+          }
+          else{
+            res.writeHead(500);
+            res.end(`Server Error: ${err.code}`);
+          }
+        } else {
+          res.writeHead(200, { "Content-Type": contentType });
+          res.end(content, "utf8");
+        }
+      });
+    }
+  }
+});
+
+const PORT = process.env.PORT || 5000;
+
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
