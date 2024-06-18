01ebe2ecce0baee64193459b3e1998fe5d4e4f53
AlessandroLF <lombardoalessandro25@hotmail.com>
2024-06-18 01:58:53 -0600

    Remove assets to be ignored

--- dbconnect.js
+++ dbconnect.js
@@ -8,25 +8,23 @@ const pool = new Pool({
 });
 
 module.exports.Create = ()=>{
-    const q = "CREATE TABLE users (id SERIAL PRIMARY KEY, name VARCHAR(50) NOT NULL, email VARCHAR(100) UNIQUE NOT NULL, date DATE NOT NULL);";
-    const result = pool.query(q, (err, res)=>{
-        if(err)
-            console.log(err);
-        else
-            console.log(res.rows);
+    const q = "CREATE TABLE users (id SERIAL PRIMARY KEY, name VARCHAR(50) NOT NULL, email VARCHAR(100) UNIQUE, public BIT NOT NULL, date TIMESTAMP DEFAULT CURRENT_TIMESTAMP);";
+    pool.query(q, (err, res)=>{
+        if(err){
+            return({err: err});
+        }else{
+            return({rows: res.rows});
+        }
     });
 }
 
-module.exports.SignIn = ()=>{
-    const q = "INSERT INTO users (name, email, date) VALUES ($1, $2, $3) RETURNING *;";
-    const now = new Date();
-    const result = pool.query(q, ["Sandro", "lombardoalessandro25@hotmail.com", now.toDateString()], (err, res)=>{
+module.exports.SignUp = (data)=>{
+    const q = "INSERT INTO users (name, email, public) VALUES ($1, $2, $3) RETURNING *;";
+    pool.query(q, data, (err, res)=>{
         if(err){
-            console.log("Err:");
-            console.log(err);
+            return({err: err});
         }else{
-            console.log('Success!');
-            console.log(res.rows);
+            return({rows: res.rows});
         }
     });
 }
